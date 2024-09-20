0a0c0d3001d45782739270536f391bc409057332
alexDev <info@alexandersstudio.uk>
2024-09-20 05:21:45 +0100

    ThemeContext Updated

--- frontend/lib/ThemeContext.js
+++ frontend/lib/ThemeContext.js
@@ -1,25 +1,64 @@
+// "use client";
+// import React, { createContext, useContext, useState, useEffect } from "react";
+
+// const ThemeContext = createContext({
+// 	theme: "light",
+// 	toggleTheme: () => {},
+// });
+
+// export const ThemeProvider = ({ children }) => {
+// 	const [theme, setTheme] = useState(() => {
+// 		const localTheme = window.localStorage.getItem("theme");
+// 		return localTheme ?? "light";
+// 	});
+
+// 	useEffect(() => {
+// 		const root = window.document.documentElement;
+
+// 		root.classList.remove(theme === "light" ? "dark" : "light");
+// 		root.classList.add(theme);
+
+// 		window.localStorage.setItem("theme", theme);
+// 	}, [theme]);
+
+// 	const toggleTheme = () => {
+// 		setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+// 	};
+
+// 	const value = { theme, toggleTheme };
+
+// 	return (
+// 		<ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+// 	);
+// };
+
+// export const useTheme = () => useContext(ThemeContext);
 "use client";
 import React, { createContext, useContext, useState, useEffect } from "react";
 
 const ThemeContext = createContext({
 	theme: "light",
-	toggleTheme: () => {},
+	toggleTheme: () => {}
 });
 
 export const ThemeProvider = ({ children }) => {
-	const [theme, setTheme] = useState(() => {
+	const [theme, setTheme] = useState("light"); // Estado inicial sin acceso a window
+
+	useEffect(() => {
+		// Acceso a localStorage solo en el lado del cliente
 		const localTheme = window.localStorage.getItem("theme");
-		return localTheme ?? "light";
-	});
+		if (localTheme) {
+			setTheme(localTheme);
+		}
+	}, []); // Se ejecuta solo una vez, al montar el componente
 
 	useEffect(() => {
 		const root = window.document.documentElement;
-
 		root.classList.remove(theme === "light" ? "dark" : "light");
 		root.classList.add(theme);
 
 		window.localStorage.setItem("theme", theme);
-	}, [theme]);
+	}, [theme]); // Actualiza el tema en localStorage y la clase en el DOM cuando cambia
 
 	const toggleTheme = () => {
 		setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
