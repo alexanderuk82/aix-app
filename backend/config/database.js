16d4d1abf871107724effc40b1019740caedc7c4
alexDev <info@alexandersstudio.uk>
2024-10-26 17:07:25 +0100

    Remove node_modules from version control

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,6 +4,31 @@ module.exports = ({ env }) => {
   const client = env("DATABASE_CLIENT", "sqlite");
 
   const connections = {
+    // mysql: {
+    //   connection: {
+    //     connectionString: env("DATABASE_URL"),
+    //     host: env("DATABASE_HOST", "localhost"),
+    //     port: env.int("DATABASE_PORT", 3306),
+    //     database: env("DATABASE_NAME", "strapi"),
+    //     user: env("DATABASE_USERNAME", "strapi"),
+    //     password: env("DATABASE_PASSWORD", "strapi"),
+    //     ssl: env.bool("DATABASE_SSL", false) && {
+    //       key: env("DATABASE_SSL_KEY", undefined),
+    //       cert: env("DATABASE_SSL_CERT", undefined),
+    //       ca: env("DATABASE_SSL_CA", undefined),
+    //       capath: env("DATABASE_SSL_CAPATH", undefined),
+    //       cipher: env("DATABASE_SSL_CIPHER", undefined),
+    //       rejectUnauthorized: env.bool(
+    //         "DATABASE_SSL_REJECT_UNAUTHORIZED",
+    //         true
+    //       ),
+    //     },
+    //   },
+    //   pool: {
+    //     min: env.int("DATABASE_POOL_MIN", 2),
+    //     max: env.int("DATABASE_POOL_MAX", 10),
+    //   },
+    // },
     mysql: {
       connection: {
         connectionString: env("DATABASE_URL"),
@@ -23,12 +48,37 @@ module.exports = ({ env }) => {
             true
           ),
         },
+        connectTimeout: 60000, // Aumentar el tiempo de espera a 60 segundos
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
         max: env.int("DATABASE_POOL_MAX", 10),
       },
     },
+    // mysql2: {
+    //   connection: {
+    //     host: env("DATABASE_HOST", "c15.tmdcloud.london"),
+    //     port: env.int("DATABASE_PORT", 3306),
+    //     database: env("DATABASE_NAME", "strapi"),
+    //     user: env("DATABASE_USERNAME", "strapi"),
+    //     password: env("DATABASE_PASSWORD", "strapi"),
+    //     ssl: env.bool("DATABASE_SSL", false) && {
+    //       key: env("DATABASE_SSL_KEY", undefined),
+    //       cert: env("DATABASE_SSL_CERT", undefined),
+    //       ca: env("DATABASE_SSL_CA", undefined),
+    //       capath: env("DATABASE_SSL_CAPATH", undefined),
+    //       cipher: env("DATABASE_SSL_CIPHER", undefined),
+    //       rejectUnauthorized: env.bool(
+    //         "DATABASE_SSL_REJECT_UNAUTHORIZED",
+    //         true
+    //       ),
+    //     },
+    //   },
+    //   pool: {
+    //     min: env.int("DATABASE_POOL_MIN", 2),
+    //     max: env.int("DATABASE_POOL_MAX", 10),
+    //   },
+    // },
     mysql2: {
       connection: {
         host: env("DATABASE_HOST", "c15.tmdcloud.london"),
@@ -47,12 +97,14 @@ module.exports = ({ env }) => {
             true
           ),
         },
+        connectTimeout: 60000, // Aumentar el tiempo de espera a 60 segundos
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
         max: env.int("DATABASE_POOL_MAX", 10),
       },
     },
+
     postgres: {
       connection: {
         connectionString: env("DATABASE_URL"),
