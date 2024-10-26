34c212cae3b459d6ea0e70dd6dfb3c83c86a33a7
alexDev <info@alexandersstudio.uk>
2024-10-26 18:58:33 +0100

    New middleware cords added

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,12 +1,19 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://aix-app.vercel.app/"], // Reemplaza con tu dominio de Vercel
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      credentials: true,
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
