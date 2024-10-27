54319a07cdaa2eab58a9d5bbc26890d99a4491e8
alexDev <info@alexandersstudio.uk>
2024-10-27 04:13:43 +0000

    Removed LocalHost URL

--- frontend/app/(pages)/results/_components/FormInterview.jsx
+++ frontend/app/(pages)/results/_components/FormInterview.jsx
@@ -16,7 +16,7 @@ import { generateSlug } from "@/lib/utils";
 import axios from "axios";
 
 const axiosClient = axios.create({
-	baseURL: "http://localhost:1337/api",
+	baseURL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
 	headers: {
 		"Content-Type": "application/json"
 	}
