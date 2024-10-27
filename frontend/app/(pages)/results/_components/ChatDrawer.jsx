36761b6f23752b6bb9a4977be5fe6aea77a3c0fb
alexDev <info@alexandersstudio.uk>
2024-10-27 04:39:02 +0000

    Chat HISTORY fixed

--- frontend/app/(pages)/results/_components/ChatDrawer.jsx
+++ frontend/app/(pages)/results/_components/ChatDrawer.jsx
@@ -33,10 +33,19 @@ const ChatDrawer = ({ recoveredText, idInterview, reset }) => {
 	const [isMobile, setIsMobile] = useState(false);
 	const [randomQuestions, setRandomQuestions] = useState([]);
 
+	// const axiosClient = axios.create({
+	// 	baseURL: "http://localhost:1337/api",
+	// 	headers: {
+	// 		"Content-Type": "application/json"
+	// 	}
+	// });
+
+	const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
+
 	const axiosClient = axios.create({
-		baseURL: "http://localhost:1337/api",
+		baseURL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
 		headers: {
-			"Content-Type": "application/json"
+			Authorization: `Bearer ${API_KEY}`
 		}
 	});
 
