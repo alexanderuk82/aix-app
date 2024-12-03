033e9ee834e0b748151c5ae90c40411bb0c028bb
alexDev <info@alexandersstudio.uk>
2024-12-03 17:24:50 +0000

    fix: kinde auth module error by adding transpilePackages config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
 	reactStrictMode: false,
 	images: {
 		domains: ["lh3.googleusercontent.com", "gravatar.com"]
-	}
+	},
+	transpilePackages: ['@kinde-oss/kinde-auth-nextjs']
 };
 
 export default nextConfig;
